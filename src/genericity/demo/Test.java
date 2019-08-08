fd14e9feb75ccc044ea874386e16b895f9e4dc51
newMeanning <nm666@foxmail.com>
2019-08-08 22:37:55 +0800

    doc

--- src/genericity/demo/Test.java
+++ src/genericity/demo/Test.java
@@ -7,21 +7,20 @@ package genericity.demo;
 
 public class Test {
     public static void main(String[] args) {
-        Pair<Integer> pairInteger =  new Pair<Integer>(1,2);
+        Pair<Integer> pairInteger =  new Pair<>(1,2);
         int one1 = pairInteger.getOne();
         int two1 = pairInteger.getTwo();
         System.out.println("one:"+one1+",two:"+two1);
 
-        Pair<Character> pairCharacter  =  new Pair<Character>('一','二');
+        Pair<Character> pairCharacter  =  new Pair<>('一','二');
         char one2 = pairCharacter.getOne();
         char two2 = pairCharacter.getTwo();
         System.out.println("one:"+one2+",two:"+two2);
 
-        Pair<String> pairString  =  new Pair<String>("I","II");
+        Pair<String> pairString  =  new Pair<>("I","II");
         String one3 = pairString.getOne();
         String two3 = pairString.getTwo();
         System.out.println("one:"+one3+",two:"+two3);
 
-        PairTwo<String,Integer> pairTwo = new PairTwo<>("牛牛",20);
     }
 }
